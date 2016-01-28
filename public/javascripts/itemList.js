a46cc36f0653fed143ded7b6de3be72e5df525fd
kaankocturk <kkocturk1@babson.edu>
2016-01-27 20:50:34 -0800

    newmvpPushingOn

--- public/javascripts/itemList.js
+++ public/javascripts/itemList.js
@@ -2,11 +2,16 @@ $(document).ready(init);
 var $mrow;
 var mrowid;
 var total = 0;
+var domstuff;
+var itemcount;
+var namesorted = false;
+var pricesorted = false;
+
 function init(){
   $('tbody').on('click', '.trash',function(e){
       e.stopPropagation();
       var $row = $(this).closest('tr');
-      var rowid = $row.attr('id').substr(4);
+      var rowid = $row.data('id');
       $.ajax({url:'/items/'+rowid, method: 'DELETE'}).success(function(data){
         console.log('data:', data);
         location.reload();
@@ -19,7 +24,8 @@ function init(){
   $('tbody').on('click', '.update',function(e){
       e.stopPropagation();
       $mrow = $(this).closest('tr');
-      mrowid = $mrow.attr('id').substr(4);
+      mrowid = $mrow.data('id');
+      console.log(mrowid);
       $('#updateItem').modal();
   });
 
@@ -30,21 +36,131 @@ function init(){
 
   $('tbody').on('click', 'tr',function(e){
     var $row = $(this).closest('tr');
-    var rowid = $row.attr('id').substr(4);
+    var rowid = $row.data('id');
     location.replace('/items/'+rowid);
+  });
+
+  $('th#names').on('click', function(){ //displays contacts alphabetically
+    var arrofnames = [];
+    var arrofsorted =[];
+    for(var i =0; i<itemcount; i++){
+      arrofnames.push($('tr#item'+i+' .name').text()+i);
+    }
+    console.log(arrofnames);
+    arrofnames.sort();
+    if(!namesorted){
+      for(var r=0; r<arrofnames.length;r++){
+          var hey = (arrofnames[r].charAt(arrofnames[r].length-1));
+          arrofsorted.push($('tr#item'+hey));
+          namesorted=true;
+      }
+    }
+    else{
+      for(var r=arrofnames.length-1; r>=0 ;r--){
+          var hey = (arrofnames[r].charAt(arrofnames[r].length-1));
+          arrofsorted.push($('tr#item'+hey));
+          namesorted=false;
+      }
+    }
+    $('tbody tr').detach();
+    $('tbody').append(arrofsorted);
+  });
+
+  $('th#prices').on('click', function(){ //displays contacts alphabetically
+    var arrofprices = [];
+    var arrofsorted =[];
+    for(var i =0; i<itemcount; i++){
+      arrofprices.push({val: parseInt($('tr#item'+i+' .price').text().substr(1)), index:i});
+    }
+    arrofprices.sort(function(a, b) {
+        var x = a.val; var y = b.val;
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+    if(!pricesorted){
+      for(var r=0; r<arrofprices.length;r++){
+          var hey = (arrofprices[r].index);
+          arrofsorted.push($('tr#item'+hey));
+          pricesorted=true;
+      }
+    }
+    else{
+      for(var r=arrofprices.length-1; r>=0 ;r--){
+          var hey = (arrofprices[r].index);
+          arrofsorted.push($('tr#item'+hey));
+          pricesorted=false;
+      }
+    }
+    $('tbody tr').detach();
+    $('tbody').append(arrofsorted);
+  });
+
+  $('.filter').click(function(e){
+    $('#filterList').modal('show');
+  });
+
+  $('.reset').click(function(e){
+    $('tbody tr').detach();
+    $('tbody').append(domstuff);
+  });
+
+  $('#filterList').on('click', '.filterList', function(e){
+    e.preventDefault();
+    var newdom = [];
+    if($('input#m2name').val()){
+      for(var i = 0 ; i< domstuff.length; i++){
+        if($(domstuff[i][0]).find('.name').text()===$('input#m2name').val()){
+          newdom.push(domstuff[i][0]);
+        }
+      }
+    }
+    else{
+      var ifcon;
+      var condition= $($(":checked")[0]).attr('id');
+      console.log(condition);
+      switch(condition){
+        case 'greater':
+          for(var i = 0 ; i< domstuff.length; i++){
+            if(ifcon = (parseInt($(domstuff[i][0]).find('.price').text().substr(1))>$('input#m2price').val())){
+              newdom.push(domstuff[i][0]);
+            }
+          }
+          break;
+        case 'less':
+          for(var i = 0 ; i< domstuff.length; i++){
+            if(ifcon = (parseInt($(domstuff[i][0]).find('.price').text().substr(1))<$('input#m2price').val())){
+              newdom.push(domstuff[i][0]);
+            }
+          }
+          break;
+        case 'equal':
+          for(var i = 0 ; i< domstuff.length; i++){
+            if(ifcon = (parseInt($(domstuff[i][0]).find('.price').text().substr(1))===$('input#m2price').val())){
+              newdom.push(domstuff[i][0]);
+            }
+          }
+          break;
+      }
+    }
+    console.log(domstuff);
+    console.log(newdom);
+    $('tbody tr').detach();
+    $('input').val('');
+    $('tbody').append(newdom);
   });
 
   $.get('/items')
     .success(function(data){
-      var domstuff = data.map(function(input, index){
+      itemcount=0;
+      domstuff = data.map(function(input, index){
       total+= input.price;
       var $button = $('<button>').addClass('btn btn-warning trash btn-sm').append('Remove item');
       var $update = $('<button>').addClass('btn btn-info update btn-sm').append('Update item');
-      var $tr = $('#template').clone().attr('id', 'item'+input._id);
+      var $tr = $('#template').clone().attr('id', 'item'+itemcount).data('id', input._id);
       $tr.find('.name').text(input.name);
       $tr.find('.price').text('$'+input.price.toFixed(2));
       $tr.find('.picurl').text(input.picurl);
       $tr.find('.remove').append($button,$update);
+      itemcount++;
       return $tr;
       });
       $('.total').text('$'+total.toFixed(2));
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 #This is zi readme
-##You can add remove and update items.
+###You can add remove and update items.
 ###You can show an item's page by clicking anywhere on the row.(except buttons)
-####yeah
+###You can sort items by name and price
+###You can filter items by name or price
+####It's 9pm, yeah.
